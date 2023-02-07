0cbfb1da90f7dca81502cb1035914cf0c0f074e4
raphaelkauan <raphaelkauanoficial@gmail.com>
2023-02-06 21:16:35 -0400

    atualizando

--- src/IdadeNoAnoQueVem.java
+++
@@ -1,7 +0,0 @@
-public class IdadeNoAnoQueVem {
-  public static void main(String[] args) {
-    int idade = 20;
-    int idadeNoAnoQueVem = idade + 1;
-    System.out.println(idadeNoAnoQueVem);
-  }
-}
