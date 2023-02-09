0e7ce4c40bde06678c0a6317ed7a417d620093e1
raphaelkauan <raphaelkauanoficial@gmail.com>
2023-02-09 15:51:15 -0400

    pequenas mudanças nas pastas

---
+++ src/raphael/estudos/primeirasemana/BoletimAprovado.java
@@ -0,0 +1,12 @@
+package raphael.estudos.primeirasemana;
+public class BoletimAprovado {
+  public static void main(String[] args) {
+    int mediaFinal = 8;
+    if (mediaFinal < 6) 
+      System.out.println("REPROVADO!");
+    else if (mediaFinal == 6)
+      System.out.println("PROVA MINERVA");
+    else 
+      System.out.println("APROVAOD!");
+  }
+}
