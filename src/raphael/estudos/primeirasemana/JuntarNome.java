49d1e494cf2ab190526dfc2dee626a7537ce756c
raphaelkauan <raphaelkauanoficial@gmail.com>
2023-02-20 14:39:19 -0400

    Novos Projetos

--- src/raphael/estudos/primeirasemana/JuntarNome.java
+++ src/raphael/estudos/primeirasemana/JuntarNome.java
@@ -1,15 +1,16 @@
 package raphael.estudos.primeirasemana;
+
 public class JuntarNome {
     public static void main(String[] args) throws Exception {
         String primeiroNome = "Rapha";
         String segundoNome = "Kauan";
-        
-        String nomeCompleto = nomeCompleto(primeiroNome,segundoNome);
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
 
         System.out.println(nomeCompleto);
     }
 
-    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
         return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
