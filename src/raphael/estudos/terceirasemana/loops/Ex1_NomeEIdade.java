c87d46f370f3159bd8f9aca2ad52ae909db3f243
raphaelkauan <raphaelkauanoficial@gmail.com>
2023-02-25 16:10:14 -0400

    programa nome e idade

--- src/raphael/estudos/terceirasemana/loops/Ex1_NomeEIdade.java
+++ src/raphael/estudos/terceirasemana/loops/Ex1_NomeEIdade.java
@@ -1,5 +1,7 @@
 package raphael.estudos.terceirasemana.loops;
 
+import java.util.Scanner;
+
 /* 
  * Faça um programa que leia conjuntos de dois valores,
  * o primeiro representando o nome do aluno e o segundo representando a sua idade.
@@ -7,5 +9,21 @@ package raphael.estudos.terceirasemana.loops;
  */
 
 public class Ex1_NomeEIdade {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
 
+    String nome;
+    int idade;
+
+    while (true) {
+      System.out.println("Nome: ");
+      nome = scan.next();
+      if (nome.equals("0"))
+        break;
+
+      System.out.println("Idade: ");
+      idade = scan.nextInt();
+    }
+    System.out.println("Continua aqui...");
+  }
 }
