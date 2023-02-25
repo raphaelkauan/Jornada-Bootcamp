bbe19382d2b52f341b8141f369898caab8195e25
raphaelkauan <raphaelkauanoficial@gmail.com>
2023-02-25 15:57:29 -0400

    iniciando a terceira semana

---
+++ src/raphael/estudos/terceirasemana/loops/Ex1_NomeEIdade.java
@@ -0,0 +1,11 @@
+package raphael.estudos.terceirasemana.loops;
+
+/* 
+ * Faça um programa que leia conjuntos de dois valores,
+ * o primeiro representando o nome do aluno e o segundo representando a sua idade.
+ * (Para inserindo o valor 0 no campo nome)
+ */
+
+public class Ex1_NomeEIdade {
+
+}
