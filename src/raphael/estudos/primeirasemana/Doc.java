b4769c8f267c2ecd30d039402dd1fbba1be28fdb
raphaelkauan <raphaelkauanoficial@gmail.com>
2023-02-25 15:56:00 -0400

    iniciando a terceira semana

---
+++ src/raphael/estudos/primeirasemana/Doc.java
@@ -0,0 +1,30 @@
+package raphael.estudos.primeirasemana;
+
+// Exemplo de documentação
+
+/**
+ * <h1>Calculadora</h1>
+ * <p>
+ * A calculadora realiza operações matemáticas entre números inteiros
+ * </p>
+ * <b>Note:</b> Leia atentamente a documentação desta classes
+ * para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author Raphael Kauan
+ * @version 1.0
+ * @since 18/02/20234
+ */
+
+public class Doc {
+  /**
+   * Este método é utilizado para somar dois números inteiros
+   * 
+   * @param numeroUm   este é o primeiro parâmetro de método
+   * @param numeroDois este é o segundo parâmetro de método
+   * @return int o resultado deste método é a soma dos dois números.
+   */
+  public int somar(int numeroUm, int numeroDois) {
+    return numeroUm + numeroDois;
+  }
+
+}
