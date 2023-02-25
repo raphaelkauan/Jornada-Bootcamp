12d37e2734051575739545536b0d0301cde43759
raphaelkauan <raphaelkauanoficial@gmail.com>
2023-02-25 19:16:54 -0400

    programa fatorial

---
+++ src/raphael/estudos/terceirasemana/loops/Ex6_Fatorial.java
@@ -0,0 +1,26 @@
+package raphael.estudos.terceirasemana.loops;
+
+import java.util.Scanner;
+
+/* 
+ * Faça um programa que calcule o fatorial de um número inteiro fornecico pelo usuário.
+ * 
+ */
+
+public class Ex6_Fatorial {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Fatorial: ");
+    int fatorial = scan.nextInt();
+
+    int multiplicacao = 1;
+
+    System.out.println(fatorial + "! = ");
+    for (int i = fatorial; i >= 1; i--) {
+      multiplicacao = multiplicacao * i;
+    }
+
+    System.out.println(multiplicacao);
+  }
+}
