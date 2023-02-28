9680e48fc42082ee224021ae3ef8633f6ba569f7
raphaelkauan <raphaelkauanoficial@gmail.com>
2023-02-28 14:35:20 -0400

    iniciando programa array multidimensional

---
+++ src/raphael/estudos/terceirasemana/arrays/Ex4_ArrayMultidimensional.java
@@ -0,0 +1,13 @@
+package raphael.estudos.terceirasemana.arrays;
+
+import java.util.Random;
+
+/* 
+ * Gere e imprima uma matriz M 4x4 com valores aleatórios entre 0-9.
+ */
+
+public class Ex4_ArrayMultidimensional {
+  public static void main(String[] args) {
+    Random random = new Random();
+  }
+}
